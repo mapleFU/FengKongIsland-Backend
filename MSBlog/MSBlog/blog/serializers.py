9c4ad30d3f280c884b27e8b8158ecdfce16e6be4
mapleFU <1506118561@qq.com>
2018-09-26 00:38:55 +0800

    [Update] Update Pipfile and ready to migrate

--- MSBlog/MSBlog/blog/serializers.py
+++ MSBlog/MSBlog/blog/serializers.py
@@ -1,10 +1,12 @@
 import typing
 
-from .models import Post, Tag, Directory
+from .models import Post, Tag, Directory, User
 from . import logger
 
 from django.db.models import Q
 from rest_framework import serializers
+from rest_framework_recursive.fields import RecursiveField
+from rest_framework.decorators import action, detail_route
 
 if typing.TYPE_CHECKING:
     from rest_framework.serializers import BaseSerializer, ModelSerializer
@@ -24,8 +26,8 @@ def handle_embeds(serializer, request, keyword: str, serialize_cls: 'BaseSeriali
     """
     embed_fields: str = request.query_params.get('embed', None)
     # 怎么处理 url 编码
-    if embed_fields is not None and 'post' in embed_fields.split(','):
-        serializer.fields['posts'] = serialize_cls(read_only=readonly,
+    if embed_fields is not None and keyword in embed_fields.replace(' ', '').split(','):
+        serializer.fields[keyword] = serialize_cls(read_only=readonly,
                                                    context=context, many=many,
                                                    validators=validators)
 
@@ -46,34 +48,58 @@ class TagSerializer(serializers.ModelSerializer):
 
 
 class PostSerializer(serializers.ModelSerializer):
-    # author = serializers.RelatedField()
-    # tags = serializers.StringRelatedField(many=True)
+    author = serializers.SlugRelatedField(slug_field='id', queryset=User.objects.all(),
+                                          allow_null=False, required=True)
+    tags = serializers.SlugRelatedField(slug_field='uuid', read_only=True, allow_null=True)
+    # tags = serializers.ManyRelatedField()
+
     def __init__(self, *args, **kwargs):
         super(PostSerializer, self).__init__(*args, **kwargs)
         request = kwargs['context']['request']
 
         handle_embeds(self, request, 'tags', TagSerializer, kwargs['context'],
                       readonly=True, many=True)
-        handle_embeds(self, request, 'directories', DirectorySerializer,
+        handle_embeds(self, request, 'directory', DirectorySerializer,
                       kwargs['context'], readonly=True, many=True)
+
+        if request.method == "POST":
+            # TODO: add validation for this field.
+            # self.validators.append()
+            self.fields['directory'] = \
+                serializers.SlugRelatedField(slug_field='id', queryset=
+                Directory.objects, allow_null=True, required=False)
 
     class Meta:
         model = Post
         fields = ('title', 'author', 'reading', 'tags')
 
 
-class DirectorySerializer(serializers.HyperlinkedModelSerializer):
+class DirectorySerializer(serializers.ModelSerializer):
+
+    def get_father_directory(self, obj):
+        if obj.father_directory is None:
+            return None
+        return DirectorySerializer(obj.father_directory, context=self.context)
+
+    def get_child_directories(self, obj):
+        return DirectorySerializer(obj.child_directories.all(), context=self.context, many=True).data
 
     def __init__(self, *args, **kwargs):
         super(DirectorySerializer, self).__init__(*args, **kwargs)
         request = kwargs['context']['request']
         if request.method == "GET":
-            handle_embeds(self, request, 'father_directory', DirectorySerializer, kwargs['context'],
-                          readonly=True, many=True)
-            handle_embeds(self, request, 'directories', DirectorySerializer,
-                          kwargs['child_directories'], readonly=True, many=True)
+            # handle_embeds(self, request, 'father_directory', serializers.PrimaryKeyRelatedField,
+            #               kwargs['context'], readonly=True)
+            embed = request.query_params.get('embed', None)
+            if embed is None:
+                return
+            embeds = embed.replace(' ', '').split(',')
+            if 'father_directory' in embeds:
+                self.fields['father_directory'] = serializers.PrimaryKeyRelatedField(read_only=True)
+            if 'child_directories' in embeds:
+                self.fields['child_directories'] = serializers.SerializerMethodField(allow_null=True)
             handle_embeds(self, request, 'posts', PostSerializer,
-                          kwargs['posts'], readonly=True, many=True)
+                          kwargs['context'], readonly=True, many=True)
         elif request.method == "POST":
             # TODO: add validation for this field.
             # self.validators.append()
--- MSBlog/MSBlog/blog/views.py
+++ MSBlog/MSBlog/blog/views.py
@@ -1,24 +1,66 @@
-from django.shortcuts import render
+from typing import TYPE_CHECKING
+import itertools
 
-from rest_framework import viewsets
+from django.shortcuts import render
+from rest_framework import viewsets, serializers, status
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, \
     ListModelMixin, UpdateModelMixin, DestroyModelMixin
 from rest_framework.permissions import AllowAny, IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
 
 from .models import Post, Tag, Directory
 from .serializers import PostSerializer, TagSerializer, DirectorySerializer
 
+if TYPE_CHECKING:
+    from .models import User
+    from typing import List
 
 INIT_CTX_FIELDS = ('GET', )
 
 
+def _get_tag_with_name(tag_name: str)->Tag:
+    raise NotImplemented("_get_tag_with_name un impl.")
+
+
 # Create your views here.
-class PostViewSet(viewsets.ReadOnlyModelViewSet):
+class PostViewSet(viewsets.ModelViewSet):
     """
     博客的VIEWSET
     """
     queryset = Post.objects.all().order_by('created_time')
     serializer_class = PostSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly, )
+
+    def create(self, request, *args, **kwargs):
+        usr: User = request.user
+        request.data['author'] = usr.id
+        tag_names = request.data['tags']
+        tags = map(_get_tag_with_name, tag_names)
+
+        avail_tags, un_avail_names, un_avail_tags = list(), list(), None
+        for tag, t_name in zip(tags, tag_names):
+            if tag is None:
+                avail_tags.append(tag)
+            else:
+                un_avail_names.append(t_name)
+        un_avail_tags = (Tag(name) for name in tag_names)
+        Tag.objects.bulk_create(un_avail_tags)
+
+        request.data['tags']: List[Tag] = map(lambda t: t.uuid, itertools.chain(avail_tags, un_avail_tags))
+        Tag.objects.bulk_create(request.data['tags'])
+        request.data['tags'] = None
+        return super(PostViewSet, self).create(request, *args, **kwargs)
+
+    def retrieve(self, request, *args, **kwargs):
+        post = self.get_object()
+        post.reading += 1
+        serialize = PostSerializer(post)
+        if serialize.is_valid():
+            post.save()
+            return Response(serialize.data, 200)
+        else:
+            return Response(serialize.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
 
 
 class TagReadViewSet(viewsets.GenericViewSet, RetrieveModelMixin, ListModelMixin):
@@ -44,16 +86,16 @@ class TagCreateViewSet(viewsets.ModelViewSet, CreateModelMixin):
     permission_classes = (IsAuthenticatedOrReadOnly, )
 
 
-class DirectoryViewSet(viewsets.ModelViewSet):
+class DirectoryViewSet(viewsets.GenericViewSet, ListModelMixin, RetrieveModelMixin):
     """
     目录的viewset
     """
-    queryset = Directory.objects.all().order_by('name')
+    queryset = Directory.objects.all()
     serializer_class = DirectorySerializer
     permission_classes = (IsAuthenticatedOrReadOnly,)
 
     def get_serializer_context(self):
         context = super().get_serializer_context()
         # 注入 request 属性
-        # context['request'] = self.request
-        return {'request': self.request}
+        context['request'] = self.request
+        return context
